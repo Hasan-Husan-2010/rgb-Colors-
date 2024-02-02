0efbfa7a05da94172d1990fb113d3fe0deab7dcc
Your Name <you@example.com>
2024-02-02 21:54:03 +0500

    Add2

--- Java.js
+++ Java.js
@@ -7,22 +7,26 @@ for(let i = 0; i < 105; i++){
     let Son = parseInt(Math.random() * 255);
     let Son2 = parseInt(Math.random() * 255);
     let Son3 = parseInt(Math.random() * 255);
-    let rgb = 'rgb('+ Son +' , '+ Son2 +' , '+ Son3 +')';
+    const rgb = 'rgb('+ Son +' , '+ Son2 +' , '+ Son3 +')';
+    const copy = structuredClone(rgb);
+    box.addEventListener('click', function(){
+        console.log(copy);
+    })
     box.style.background = rgb ;
     box.textContent = rgb;
     btn.style.background = rgb;
-    const copy = structuredClone(rgb)
     boxs.appendChild(box)
     btn.addEventListener('click', function(){
-    for(let i = 0; i < 105; i++){
     let Son = parseInt(Math.random() * 255);
     let Son2 = parseInt(Math.random() * 255);
     let Son3 = parseInt(Math.random() * 255);
-    let rgb = 'rgb('+ Son +' , '+ Son2 +' , '+ Son3 +')';
+    const rgb = 'rgb('+ Son +' , '+ Son2 +' , '+ Son3 +')';
     box.style.background = rgb ;
     box.textContent = rgb;
     btn.style.background = rgb;
-    boxs.appendChild(box)
-    }
-    });
+    const copy = structuredClone(rgb);
+    box.addEventListener('click', function(){
+        console.log(copy);
+    })
+});
 }

ab2ce024b5c80df7bf112db7f3cd55dd4d2e51ed
Your Name <you@example.com>
2024-02-02 09:50:35 +0500

    add

--- Java.js
+++ Java.js
@@ -7,18 +7,21 @@ for(let i = 0; i < 105; i++){
     let Son = parseInt(Math.random() * 255);
     let Son2 = parseInt(Math.random() * 255);
     let Son3 = parseInt(Math.random() * 255);
-    box.style.background = 'rgb('+ Son +' '+ Son2 +' '+ Son3 +')';
-    box.textContent = 'rgb('+ Son +', '+ Son2 +', '+ Son3 +')';
-    btn.style.background = 'rgb('+ Son +' '+ Son2 +' '+ Son3 +')';
+    let rgb = 'rgb('+ Son +' , '+ Son2 +' , '+ Son3 +')';
+    box.style.background = rgb ;
+    box.textContent = rgb;
+    btn.style.background = rgb;
+    const copy = structuredClone(rgb)
     boxs.appendChild(box)
     btn.addEventListener('click', function(){
     for(let i = 0; i < 105; i++){
     let Son = parseInt(Math.random() * 255);
     let Son2 = parseInt(Math.random() * 255);
     let Son3 = parseInt(Math.random() * 255);
-    box.style.background = 'rgb('+ Son +' '+ Son2 +' '+ Son3 +')';
-    box.textContent = 'rgb('+ Son +', '+ Son2 +', '+ Son3 +')';
-    btn.style.background = 'rgb('+ Son +' '+ Son2 +' '+ Son3 +')';
+    let rgb = 'rgb('+ Son +' , '+ Son2 +' , '+ Son3 +')';
+    box.style.background = rgb ;
+    box.textContent = rgb;
+    btn.style.background = rgb;
     boxs.appendChild(box)
     }
     });
